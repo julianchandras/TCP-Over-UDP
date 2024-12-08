28d7e94c95a38b927f351b811620431267245846
Venantius Sean Ardi Nugroho <seanardi4@gmail.com>
2024-12-08 22:09:54 +0700

    feat: file input

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,10 +1,17 @@
-#ifndef utils_h
-#define utils_h
+#ifndef UTILS_H
+#define UTILS_H
 
 #include "segment.hpp"
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <vector>
 
 uint8_t *serializeSegment(Segment *toBeSent, size_t optionsLen, size_t payloadLen);
 void deserializeToSegment(Segment *segment, uint8_t *buffer, size_t nBytes);
 void printSegment(const Segment &seg, size_t payloadLen);
+// void encodeFile(const std::string &filePath, uint8_t &dataStream);
 
 #endif
